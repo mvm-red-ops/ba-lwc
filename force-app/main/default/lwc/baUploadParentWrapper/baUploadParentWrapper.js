e68a2c5b7bd0b2e04497708759c692f9265a3bc3
taylor <tayloryoung@mvmediasales.com>
2021-03-30 11:09:05 -0400

    refactor 1 succesful upload and data table display

--- force-app/main/default/lwc/baUploadParentWrapper/baUploadParentWrapper.js
+++ force-app/main/default/lwc/baUploadParentWrapper/baUploadParentWrapper.js
@@ -1,10 +1,13 @@
 import { LightningElement, track } from 'lwc';
 
 export default class BaUploadParentWrapper extends LightningElement {
-  @track dealProgramSelected
-  @track dealProgram
-  @track fileUploaded
-  @track csvDownloadReady
+  dealProgramSelected
+  dealProgram
+  fileUploaded
+  csvDownloadReady
+  documentId
+  toggleSpinner
+  
 
   //handles selection of deal program from dropdown
   handleDealProgramSelection(event){
@@ -12,14 +15,31 @@ export default class BaUploadParentWrapper extends LightningElement {
     this.dealProgramSelected = true
   }
 
-  //completely resets state to initial values, used mainly for reselecting deal program
-  resetState(event){
-   this.dealProgramSelected = null
-   this.dealProgram = null
-   this.fileUploaded = null
-   this.csvDownloadReady = null
+  handleFileUpload(event){
+    const fileObj = event.detail
+    window.console.log(`fileObj: ${JSON.stringify(fileObj.detail)}`)
+    const fileObj2 = fileObj.detail.files[0]
+    window.console.log(`fileObj2: ${fileObj2}`)
+
+    this.documentId = fileObj2.documentId
+    window.console.log(`documentId: ${fileObj.documentId}`)
   }
-//trial comment
+
+  //handle events to change spinner state
+  spinnerHandler(event){
+    if(event.detail === 'show') this.toggleSpinner = true
+    if(event.detail === 'hide') this.toggleSpinner = false
+  }
+
+    //completely resets state to initial values, used mainly for reselecting deal program
+    resetState(event){
+      this.dealProgramSelected = null
+      this.dealProgram = null
+      this.documentId = null
+      this.toggleSpinner = false
+      this.fileUploaded = null
+      this.csvDownloadReady = null
+     }
 }
 
 
